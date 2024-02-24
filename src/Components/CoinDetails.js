0d5db1f4dd88a9ee05add39186af2aaf0983aa89
rimsha-hanif24 <rimhsa.hanif@techtronix.biz>
2024-02-25 00:25:06 +0500

    some changes

--- src/Components/CoinDetails.js
+++ src/Components/CoinDetails.js
@@ -18,53 +18,55 @@ const CoinDetails = () => {
     (state) => state.getCoinDetails
   );
 
+  if (loading) {
+    return <Loader />;
+  }
+
+  if (error || !coinDetail) {
+    return <ErrorPage error={error} />;
+  }
+
+  const renderImage = coinDetail.image && coinDetail.image.large;
+
   return (
     <div className="text-white flex items-center p-2">
-      {loading ? (
-        <Loader />
-      ) : error ? (
-        <ErrorPage error={error} />
-      ) : (
-        <div className="grid md:grid-cols-6 grid-cols-1 w-full">
-          {/* Left Side */}
-          <div className="md:col-span-2 col-span-1 flex flex-col justify-center border p-2">
-            <div className="flex gap-4 my-4">
-              <button
-                className="text-white px-1 rounded bg-gradient-to-r from-purple-500 to-pink-500"
-                onClick={() => {}}
-              >
-                USD
-              </button>
-              <button
-                className="text-white px-1 rounded bg-gradient-to-r from-purple-500 to-pink-500"
-                onClick={() => {}}
-              >
-                PKR
-              </button>
-            </div>
-            <div>
-              <div className="flex justify-between items-center">
-                <h1 className="text-tellow-600 font-bold text-[30px] text-yellow-700">
-                  {coinDetail.name}
-                </h1>
-                <h1 className="flex items-center gap-2">
-                  <span>
-                    <TbActivityHeartbeat className="text-yellow-600 text-[25px]" />
-                  </span>
-                  <span className="text-[30px] font-bold">
-                    #{coinDetail.market_cap_rank}
-                  </span>
-                </h1>
-              </div>
-              <img src={coinDetail.image.large} alt="img" width={"150px"} />
-            </div>
+      <div className="grid md:grid-cols-6 grid-cols-1 w-full">
+        <div className="md:col-span-2 col-span-1 flex flex-col justify-center border p-2">
+          <div className="flex gap-4 my-4">
+            <button
+              className="text-white px-1 rounded bg-gradient-to-r from-purple-500 to-pink-500"
+              onClick={() => {}}
+            >
+              USD
+            </button>
+            <button
+              className="text-white px-1 rounded bg-gradient-to-r from-purple-500 to-pink-500"
+              onClick={() => {}}
+            >
+              PKR
+            </button>
           </div>
-          {/* Right side - graph */}
-          <div className="md:col-span-4 col-span-1 flex justify-center items-center border">
-            <h1>Graph</h1>
+          <div>
+            <div className="flex justify-between items-center">
+              <h1 className=" font-bold text-[30px] text-yellow-700">
+                {coinDetail.name}
+              </h1>
+              <h1 className="flex items-center gap-2">
+                <span>
+                  <TbActivityHeartbeat className="text-yellow-600 text-[25px]" />
+                </span>
+                <span className="text-[30px] font-bold">
+                  #{coinDetail.market_cap_rank}
+                </span>
+              </h1>
+            </div>
+            {renderImage && <img src={renderImage} alt="img" width={"150px"} />}
           </div>
         </div>
-      )}
+        <div className="md:col-span-4 col-span-1 flex justify-center items-center border">
+          <h1>Graph</h1>
+        </div>
+      </div>
     </div>
   );
 };
